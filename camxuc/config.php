dbf95ea999525d2626857f1470675e4aecdce641
Huỳnh Đình Quí <deathbook1999@gmail.com>
2017-06-11 15:20:18 +0700

    bot camxuc
    Signed-off-by: Huỳnh Đình Quí <deathbook1999@gmail.com>

---
+++ camxuc/config.php
@@ -0,0 +1,21 @@
+<?php
+$host = "db4free.net:3307";
+$username = "shine911";
+$password = "thiennhi1412";	
+$dbname = "botlove";
+
+
+$connection = mysql_connect($host,$username,$password);
+
+if (!$connection)
+
+{
+
+die('Could not connect: ' . mysql_error());
+
+}
+
+mysql_select_db($dbname) or die(mysql_error());
+
+mysql_query("SET NAMES utf8");
+?>
---
+++ camxuc/lien-he-admin/index.php
@@ -0,0 +1,4 @@
+<?php
+$URL="https://thbao.me";
+header ("Location: $URL");
+?>
